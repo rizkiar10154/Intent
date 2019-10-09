505d76ee28fec3ea66d02ee5188fa83c5eec7a69
rizkiar10154 <rizkyarif196@gmail.com>
2019-10-09 14:33:04 +0700

    Testing App Purposes

---
+++ app/src/main/java/com/example/intent/Main2Activity.kt
@@ -0,0 +1,23 @@
+package com.example.intent
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main2.*
+
+class Main2Activity : AppCompatActivity() {
+
+    val id:Int = 10
+    val language:String = "kotlin"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+        buttona2.setOnClickListener() {
+            intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("id_value", id)
+            intent.putExtra("language_value", language)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/intent/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.intent
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    val id:Int = 10
+    val language:String = "kotlin"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        buttona1.setOnClickListener() {
+                intent = Intent(this, Main2Activity::class.java)
+            intent.putExtra("id_value", id)
+                    intent.putExtra("language_value", language)
+            startActivity(intent)
+        }
+    }
+}
